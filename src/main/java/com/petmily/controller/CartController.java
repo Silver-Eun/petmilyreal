e6cc5fb4d083d33db48f2dcae7d346d0a1c708e2
Silver-Eun <h22sang@naver.com>
2024-11-04 21:24:25 +0900

    jar

--- src/main/java/com/petmily/controller/CartController.java
+++ src/main/java/com/petmily/controller/CartController.java
@@ -17,7 +17,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping("/cart")
--- src/main/java/com/petmily/controller/RestBoardController.java
+++ src/main/java/com/petmily/controller/RestBoardController.java
@@ -12,8 +12,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import java.io.File;
 import java.io.IOException;
 import java.util.HashMap;
