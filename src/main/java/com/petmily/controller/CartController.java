28e9a691216b264af9cd152165efa4aa822e2a18
Silver-Eun <h22sang@naver.com>
2024-11-04 20:32:32 +0900

    change springboot version

--- src/main/java/com/petmily/controller/CartController.java
+++ src/main/java/com/petmily/controller/CartController.java
@@ -17,7 +17,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import jakarta.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping("/cart")
