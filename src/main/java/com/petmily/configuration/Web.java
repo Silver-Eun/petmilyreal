a5c5b46af53e84ac74e2d5806feac05f9cc6256b
Silver-Eun <h22sang@naver.com>
2024-11-07 21:59:45 +0900

    delete html

--- src/main/java/com/petmily/configuration/Web.java
+++ src/main/java/com/petmily/configuration/Web.java
@@ -1,20 +1,16 @@
 package com.petmily.configuration;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.thymeleaf.templatemode.TemplateMode;
-import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
 
 @Configuration
 public class Web implements WebMvcConfigurer {
 
-//	@Override
-//	public void addViewControllers(ViewControllerRegistry registry) {
-//		registry.addRedirectViewController("/", "home");
-//
-//	}
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addRedirectViewController("/", "home");
+
+	}
 
 }
--- src/main/java/com/petmily/controller/HomeController.java
+++ src/main/java/com/petmily/controller/HomeController.java
@@ -6,11 +6,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
-	@GetMapping(value = "/")
-	public String homePage() {
-		return "index";
-	}
-
 	@GetMapping(value = "/home")
 	public String home() {
 		return "home";
