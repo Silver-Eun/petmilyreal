cc5927755c6329a053a65c4d0cffe4b82189bc57
Silver-Eun <h22sang@naver.com>
2024-11-19 17:40:02 +0900

    connect to netlify

--- src/main/java/com/petmily/configuration/Web.java
+++ src/main/java/com/petmily/configuration/Web.java
@@ -1,6 +1,7 @@
 package com.petmily.configuration;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -11,6 +12,14 @@ public class Web implements WebMvcConfigurer {
 	public void addViewControllers(ViewControllerRegistry registry) {
 		registry.addRedirectViewController("/", "home");
 
+	}
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+				.allowedOrigins("https://petmilyreal.netlify.app") // Netlify 도메인
+				.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+				.allowCredentials(true);
 	}
 
 }
