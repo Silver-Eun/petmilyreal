b32b4e181d7d1f8ab2ed20b4456d1db6aca29d13
Silver-Eun <h22sang@naver.com>
2024-11-20 14:00:58 +0900

    edit userUpdate

--- src/main/java/com/petmily/PetmilyRealApplication.java
+++ src/main/java/com/petmily/PetmilyRealApplication.java
@@ -5,11 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
-public class PetmilyFinalFinalRealFinalApplication {
+public class PetmilyRealApplication {
 
     public static void main(String[] args) {
 
-        SpringApplication.run(PetmilyFinalFinalRealFinalApplication.class, args);
+        SpringApplication.run(PetmilyRealApplication.class, args);
     }
 
 }
--- src/main/resources/static/javascript/update.js
+++ src/main/resources/static/javascript/update.js
@@ -5,11 +5,11 @@ function Userupdate(id) {
 
     axios.get(url)
         .then(response => {
-            console.log("** 회원정보 수정 성공 **");
+            alert("회원정보 수정 성공");
             document.getElementById('resultArea1').innerHTML = response.data;
         })
         .catch(err => {
-            alert("** 회원정보 수정 실패 => " + err.message);
+            alert("회원정보 수정 실패 : " + err.message);
         });
 
     document.getElementById('resultArea1').innerHTML = "";
